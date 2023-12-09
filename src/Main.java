7ffa9b3bd68a0b7cfe0442debe379174073595c5
Taras Stepaniuk <taras.stepaniuk@gmail.com>
2023-12-09 11:18:33 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Fraction one = new Fraction(2, 4);
+        Fraction two = new Fraction(4, 8);
+
+        System.out.println(one.equals(two));
+    }
+}
